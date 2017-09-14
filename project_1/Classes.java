70e6209e5852c287a2e5d591d6b160765f23c4c7
Anthony Phillips <aphillips196@gmail.com>
2017-09-14 13:33:29 -0400

    Formatting

--- project_1/Classes.java
+++ project_1/Classes.java
@@ -1,64 +1,45 @@
 public class Classes {
-    public static void main(String[] args) {
-        Die[] dice = new Die[20];
 
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = new Die();
-            dice[i].roll();
-        }
+   public static void main(String[] args) {
+      Die[] dice = new Die[2];
 
-        for (Die die : dice) {
-            die.printFace();
-        }
-    }
+      for (int i = 0; i < dice.length; i++) {
+         dice[i] = new Die();
+         dice[i].roll();
+      }
 
-    public static class Die {
-        private int value;
-        public int getValue() {
-            return this.value;
-        }
-        public void setValue(int value) {
-            this.value = value;
-        }
+      for (Die die : dice)
+         die.printFace();
 
-        public Die() {
-            this.value = -1;
-        }
+   }
 
-        public Die(int value) {
-            this.value = value;
-        }
+   public static class Die {
+      private int value;
+      public int getValue() { return this.value; }
+      public void setValue(int value) { this.value = value; }
 
-        public void roll() {
-            int randomInt = (int)(Math.random() * 5) + 1;
-            this.value = randomInt;
-        }
+      public Die() {
+         this.value = -1;
+      }
 
-        public void printFace() {
-            System.out.println();
-            switch (this.value) {
-                case 1:
-                    System.out.println("\n  *  \n");
-                    break;
-                case 2:
-                    System.out.println("*    \n     \n    *");
-                    break;
-                case 3:
-                    System.out.println("*    \n  *  \n    *");
-                    break;
-                case 4:
-                    System.out.println("*   *\n     \n*   *");
-                    break;
-                case 5:
-                    System.out.println("*   *\n  *  \n*   *");
-                    break;
-                case 6:
-                    System.out.println("* * *\n     \n* * *");
-                    break;
-                default:
-                    System.out.println("No value.");
-            }
-            System.out.println();
-        }
-    }
+      public void roll() {
+         int randomInt = (int)(Math.random() * 5) + 1;
+         this.value = randomInt;
+      }
+
+      public void printFace() {
+         System.out.println();
+         switch (this.value) {
+            case 1:  System.out.println("     \n  *  \n     "); break;
+            case 2:  System.out.println("*    \n     \n    *"); break;
+            case 3:  System.out.println("*    \n  *  \n    *"); break;
+            case 4:  System.out.println("*   *\n     \n*   *"); break;
+            case 5:  System.out.println("*   *\n  *  \n*   *"); break;
+            case 6:  System.out.println("* * *\n     \n* * *"); break;
+            default: System.out.println("No value.");           break;
+         }
+         System.out.println();
+      }
+   }
+
 }
--- project_1/SecretNumber.java
+++ project_1/SecretNumber.java
@@ -2,52 +2,54 @@ import java.util.Scanner;
 
 class SecretNumber {
 
-    public static void main(String[] args) {
-        int secret = (int)(Math.random()*99) + 1;
-        Scanner stdIn = new Scanner(System.in);
+   public static void main(String[] args) {
+      int secret = (int)(Math.random()*99) + 1;
+      Scanner stdIn = new Scanner(System.in);
 
-        System.out.println("Guess the secret number. Press 0 to exit.");
-        while (true) {
-            int guess = 0;
+      System.out.println("Guess the secret number. Enter 0 to exit.");
+      while (true) {
+         int guess = 0;
 
-            System.out.print("Your guess: ");
+         System.out.print("Your guess: ");
 
-            try {
-                guess = stdIn.nextInt();
-            } catch (Exception ex) {
-                System.out.println("The secret number is an integer. Try again.");
-                stdIn.nextLine();
-                continue;
-            }
+         try {
+            guess = stdIn.nextInt();
+         } catch (Exception ex) {
+            System.out.println("The secret number is an integer. Try again.");
+            stdIn.nextLine();
+            continue;
+         }
 
-            if (guess == 0) {
-                System.out.println("User quit. Secret was " + secret);
-                break;
-            }
+         if (guess == 0) {
+            System.out.println("User quit. Secret was " + secret);
+            break;
+         }
 
-            if (guess < 1 || guess > 100)
-                System.out.println("The secret number is within the range of 1-100. Try again.");
+         if (guess < 1 || guess > 100)
+            System.out.println("The secret number is within the range of 1-100."
+               + " Try again.");
 
-            if (guess != secret) {
-                boolean isLow = secret - guess > 0 ? true : false;
-                int difference = Math.abs(secret - guess);
-                String intensifier;
+         if (guess != secret) {
+            boolean isLow = secret - guess > 0 ? true : false;
+            int difference = Math.abs(secret - guess);
+            String intensifier;
 
-                if (difference > 30)
-                    intensifier = "way too";
-                else if (difference > 10)
-                    intensifier = "too";
-                else
-                    intensifier = "a little";
+            if (difference > 30)
+            intensifier = "way too";
+            else if (difference > 10)
+            intensifier = "too";
+            else
+            intensifier = "a little";
 
-                System.out.println("Your guess was " + intensifier + (isLow ? " low." : " high."));
-            }
+            System.out.println("Your guess was " + intensifier
+               + (isLow ? " low." : " high."));
+         }
 
-            if (guess == secret) {
-                System.out.println("You guessed the secret number!");
-                break;
-            }
-        }
-    }
+         if (guess == secret) {
+            System.out.println("You guessed the secret number!");
+            break;
+         }
+      }
+   }
 
 }
